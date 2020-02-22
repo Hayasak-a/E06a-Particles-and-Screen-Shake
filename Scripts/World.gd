965738e7521fd473ea5a9ac67be1030e20611489
Hayasak-a <59937605+Hayasak-a@users.noreply.github.com>
2020-02-21 19:05:56 -0500

    Completion
    Completed the probject.

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,13 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	$Camera._ready()
+	if Input.is_action_pressed("left"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_pressed("right"):
+		$Camera.add_trauma(1)
+	if Input.is_action_pressed("up"):
+		$Camera.add_trauma(.35)
+	if Input.is_action_pressed("down"):
+		$Camera.add_trauma(.65)
+	$Camera._apply_shake()
